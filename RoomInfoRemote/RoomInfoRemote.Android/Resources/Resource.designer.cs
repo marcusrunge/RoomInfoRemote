93d4aedb4aeeceb85fff839d8f3166aabf7e906d
Marcus Runge <marcus.runge@outlook.com>
2019-02-09 17:30:02 +0100

    CHANGED iOS Build target

--- RoomInfoRemote/RoomInfoRemote.Android/Resources/Resource.designer.cs
+++ RoomInfoRemote/RoomInfoRemote.Android/Resources/Resource.designer.cs
@@ -8951,10 +8951,10 @@ namespace RoomInfoRemote.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int ic_launcher_circle = 2130903040;
+			public const int ic_launcher = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int ic_launcher_square = 2130903041;
+			public const int ic_launcher_round = 2130903041;
 			
 			static Mipmap()
 			{
