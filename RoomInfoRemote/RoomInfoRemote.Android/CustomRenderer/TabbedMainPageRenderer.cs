a5ae3b542c868ab99b99c71dfbde6c1487d398b8
Marcus Runge <marcus.runge@outlook.com>
2019-02-11 19:37:43 +0100

    ADDED Divider and Shadow

---
+++ RoomInfoRemote/RoomInfoRemote.Android/CustomRenderer/TabbedMainPageRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using RoomInfoRemote.Droid.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedMainPageRenderer))]
+namespace RoomInfoRemote.Droid.CustomRenderer
+{
+    public class TabbedMainPageRenderer : TabbedPageRenderer
+    {
+        public TabbedMainPageRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+            ViewGroup layout = (ViewGroup)this.GetChildAt(0);
+            BottomNavigationView bottomNavigationView = (BottomNavigationView)layout.GetChildAt(1);
+            Android.Views.View topShadow = LayoutInflater.From(Context).Inflate(Resource.Layout.top_shadow, null);
+
+            Android.Widget.RelativeLayout.LayoutParams layoutParams =
+                new Android.Widget.RelativeLayout.LayoutParams(LayoutParams.MatchParent, 15);
+            layoutParams.AddRule(LayoutRules.Above, bottomNavigationView.Id);
+
+            layout.AddView(topShadow, 2, layoutParams);
+        }
+    }
+}
--- RoomInfoRemote/RoomInfoRemote.Android/Resources/Resource.designer.cs
+++ RoomInfoRemote/RoomInfoRemote.Android/Resources/Resource.designer.cs
@@ -7022,26 +7022,26 @@ namespace RoomInfoRemote.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020148
-			public const int avd_hide_password_1 = 2130837832;
-			
 			// aapt resource value: 0x7f020149
-			public const int avd_hide_password_2 = 2130837833;
+			public const int avd_hide_password_1 = 2130837833;
 			
 			// aapt resource value: 0x7f02014a
-			public const int avd_hide_password_3 = 2130837834;
+			public const int avd_hide_password_2 = 2130837834;
+			
+			// aapt resource value: 0x7f02014b
+			public const int avd_hide_password_3 = 2130837835;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02014b
-			public const int avd_show_password_1 = 2130837835;
-			
 			// aapt resource value: 0x7f02014c
-			public const int avd_show_password_2 = 2130837836;
+			public const int avd_show_password_1 = 2130837836;
 			
 			// aapt resource value: 0x7f02014d
-			public const int avd_show_password_3 = 2130837837;
+			public const int avd_show_password_2 = 2130837837;
+			
+			// aapt resource value: 0x7f02014e
+			public const int avd_show_password_3 = 2130837838;
 			
 			// aapt resource value: 0x7f020056
 			public const int btn_checkbox = 2130837590;
@@ -7751,11 +7751,11 @@ namespace RoomInfoRemote.Droid
 			// aapt resource value: 0x7f020141
 			public const int notification_icon_background = 2130837825;
 			
-			// aapt resource value: 0x7f020146
-			public const int notification_template_icon_bg = 2130837830;
-			
 			// aapt resource value: 0x7f020147
-			public const int notification_template_icon_low_bg = 2130837831;
+			public const int notification_template_icon_bg = 2130837831;
+			
+			// aapt resource value: 0x7f020148
+			public const int notification_template_icon_low_bg = 2130837832;
 			
 			// aapt resource value: 0x7f020142
 			public const int notification_tile_bg = 2130837826;
@@ -7764,10 +7764,13 @@ namespace RoomInfoRemote.Droid
 			public const int notify_panel_notification_icon_bg = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int tooltip_frame_dark = 2130837828;
+			public const int shadow = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int tooltip_frame_light = 2130837829;
+			public const int tooltip_frame_dark = 2130837829;
+			
+			// aapt resource value: 0x7f020146
+			public const int tooltip_frame_light = 2130837830;
 			
 			static Drawable()
 			{
@@ -8954,6 +8957,9 @@ namespace RoomInfoRemote.Droid
 			
 			// aapt resource value: 0x7f040056
 			public const int Toolbar = 2130968662;
+			
+			// aapt resource value: 0x7f040057
+			public const int top_shadow = 2130968663;
 			
 			static Layout()
 			{
