50e8131fc8a49761f794e2ce82646ab450180a5b
Marcus Runge <marcus.runge@outlook.com>
2019-11-03 17:25:44 +0100

    UPDATED SF Lic

--- RoomInfoRemote/RoomInfoRemote/App.xaml.cs
+++ RoomInfoRemote/RoomInfoRemote/App.xaml.cs
@@ -23,7 +23,7 @@ namespace RoomInfoRemote
 
         protected override async void OnInitialized()
         {
-            SyncfusionLicenseProvider.RegisterLicense("MTM0Nzg4QDMxMzcyZTMyMmUzMEZQNmNuZ1VtQ1V0QjNpWVdKR09pVm84QUVERVJIU3lGOWZlYzRFOGg1U289");
+            SyncfusionLicenseProvider.RegisterLicense("MTY1ODIyQDMxMzcyZTMzMmUzMFQ3cmZtUUx4MURUdGJadytTUkdrS2FadG9ybndNRU1zRU1VeWc3Yk9adEE9");
             InitializeComponent();            
             if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
             {
